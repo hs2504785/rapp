4f43b777171db4cbc7aed70076f838920a51df8c
Hemant Singh <hemantsingh@Hemants-Air.lan>
2022-08-13 10:49:36 +0530

    Hello Component added

---
+++ src/User.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function User() {
+  return (
+    <div>
+      User
+      <h2>
+        <section>
+          <h2>
+            My Name is <i class="fas fa-khanda    "></i>
+          </h2>
+          <ul>
+            <li>Ij</li>
+            <li>nn</li>
+            <li></li>
+            <li>njn</li>
+            <li>jbb</li>
+          </ul>
+        </section>
+      </h2>
+    </div>
+  );
+}
